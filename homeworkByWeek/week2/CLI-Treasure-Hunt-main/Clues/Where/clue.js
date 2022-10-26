842c350592d62b2baa4826187d0dd07296888986
tony-dev-wm <ta.devopsuk@gmail.com>
2022-10-26 21:29:16 +0100

    d

--- homeworkByWeek/week2/CLI-Treasure-Hunt-main/Clues/Where/clue.js
+++
@@ -1,5 +0,0 @@
-console.log(`You got here by typing a relative path.
-Learn more about changing directory here: 
-https://alligator.io/workflow/command-line-basics-changing-directories/
-Now change directory to ./How and get your next clue.
-`)
--- homeworkByWeek/week2/CLI-Treasure-Hunt-main/Clues/Who/help.js
+++
@@ -1,4 +0,0 @@
-console.log(`How do you move a file? 
-Try typing mv into the command line. 
-What happens? 
-What should you google to figure out the right command?`)
